46440eb9813fa39aa01a0ff8dfa6511c25f42596
nikoskef <nikoskef86@gmail.com>
2018-10-04 16:05:25 +0300

    added CORS

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from flask_cors import CORS
 
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.room import Room, RoomList, RoomCreate
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secret'
