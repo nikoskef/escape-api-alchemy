80537d64258d2ab2f9caad0472aa08f803858fe1
nikoskef <nikoskef86@gmail.com>
2018-10-10 18:36:35 +0300

    update

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import os
 
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
-from security import authenticate, identity
 from resources.user import UserRegister
 from resources.room import Room, RoomList, RoomCreate
 
@@ -13,10 +12,12 @@ app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.secret_key = 'secret'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWTManager(app)
 
 api.add_resource(Room, '/room/<int:_id>')
 api.add_resource(RoomCreate, '/room/create')
